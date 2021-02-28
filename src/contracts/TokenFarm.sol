7d00f0f0b1f7891e748de6ee5b2a3efe42a32088
Farzan <farzanpira3@gmail.com>
2021-03-01 00:27:24 +0530

    1hr 18min

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -8,8 +8,35 @@ contract TokenFarm {
     DappToken public dappToken;
     DaiToken public daiToken;
 
+    address[] public stakers;
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
+
     constructor(DappToken _dappToken, DaiToken _daiToken) public{
         dappToken = _dappToken;
         daiToken = _daiToken;
     }
+
+    // 1. Staking Tokens (Deposit)
+    function stakeTokens(uint _amount) public {
+        // Transfer DAI Tokens to this contract
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+
+        // Update Staking balance
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+        // Add user to the stakers array, if they have not staked
+        if(!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+
+        // Update staking status
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
+    }
+
+    // 2. Unstaking Tokens (Withdraw)
+
+    // 3. Issuing Tokens
 }
